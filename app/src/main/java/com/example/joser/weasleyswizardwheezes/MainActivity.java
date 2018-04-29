bc346e6598554417944a7da9ec4c959171b994bf
Jose Ramirez <josecrmrz@gmail.com>
2018-04-29 01:10:28 -0500

    Add hyperlink to location

--- app/src/main/java/com/example/joser/weasleyswizardwheezes/MainActivity.java
+++ app/src/main/java/com/example/joser/weasleyswizardwheezes/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.joser.weasleyswizardwheezes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvAddress = findViewById(R.id.tvAddress);
+        tvAddress.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
